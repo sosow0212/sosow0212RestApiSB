a70dacdcddb0825a1a7d198f2de0ce8945a58426
sosow0212 <sosow0212@naver.com>
2021-09-29 20:36:15 +0900

    Restful API 완성

--- src/main/java/com/sosow0212/ApiController.java
+++ src/main/java/com/sosow0212/ApiController.java
@@ -32,8 +32,26 @@ public class ApiController {
         return new ArrayList<UserProfile>(userMap.values());
     }
 
+    @PutMapping("/user/{id}") // Postman Put 방식으로 추가 가능
+    public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+        UserProfile userProfile = new UserProfile(id, name, phone, address);
+        userMap.put(id, userProfile);
+    }
+
+    @PostMapping("/user/{id}") // post방식으로 수정
+    public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+        UserProfile userProfile = userMap.get(id); // id를 찾는다.
+        userProfile.setName(name); // 이름 바꾸기
+        userProfile.setPhone(phone);
+        userProfile.setAddress(address);
+    }
+
+    @DeleteMapping("/user/{id}") // delete 방식으로 삭제
+    public void deleteUserProfile(@PathVariable("id") String id) {
+        userMap.remove(id);
+    }
 /////////////////////////////////////////////////////////////////////////////
-    // 아래부터 postman
+    // 아래부터 간단한 방식
 
     @RequestMapping(value = "/api/test", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK) // 정상적으로 받으면 OK
