7e5d4f37c0bdb87caa25eddfbed6586782a4df9f
sosow0212 <sosow0212@naver.com>
2021-09-29 20:06:40 +0900

    Request-test

---
+++ src/main/java/com/sosow0212/ApiController.java
@@ -0,0 +1,29 @@
+package com.sosow0212;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @RequestMapping(value = "/api/test", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK) // 정상적으로 받으면 OK
+    public String getApiTest() {
+
+        return "{\"result\":\"ok\"}";
+    }
+    // http://localhost:8080/api/test
+
+
+    @RequestMapping(value = "/api/test2", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK) // 정상적으로 받으면 OK
+    public String getApiTest2() {
+
+        return "{\"result\":\"ok2\"}";
+    }
+    // http://localhost:8080/api/test2
+
+}
--- src/main/java/com/sosow0212/startApp.java
+++ src/main/java/com/sosow0212/startApp.java
@@ -1,7 +1,13 @@
 package com.sosow0212;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트라는 걸 알려주는 어노테이션
 public class startApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(startApp.class, args); // 스프링 어플리케이션 실행하겠다는 뜻
+
+    }
 }
